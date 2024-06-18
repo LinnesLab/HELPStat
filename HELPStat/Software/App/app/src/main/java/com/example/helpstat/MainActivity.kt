21e2e64d8c38d24844b8695f8230cce9dce1918b
S Riegle <43761819+SRiegle2020@users.noreply.github.com>
2024-06-18 18:01:02 -0400

    Started (hopefully) implementing BLE
    Started following a tutorial at: https://punchthrough.com/android-ble-guide/. Found that his seems to be the "gold-standard" tutorial for setting up BLE on Android. Will also be referring to Android Developer's documentation regarding BLE at: https://developer.android.com/develop/connectivity/bluetooth/ble/ble-overview.

--- HELPStat/Software/App/app/src/main/java/com/example/helpstat/MainActivity.kt
+++ HELPStat/Software/App/app/src/main/java/com/example/helpstat/MainActivity.kt
@@ -1,28 +1,113 @@
 package com.example.helpstat
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.helpstat.ui.theme.HELPStatTheme
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+
+private const val PERMISSION_REQUEST_CODE = 1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button_Start)
+        findViewById<Button>(R.id.button_connect)
             .setOnClickListener {
-                Log.d("BUTTONS","User hit START")
+                startBleScan()
             }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != PERMISSION_REQUEST_CODE) return
+        val containsPermanentDenial = permissions.zip(grantResults.toTypedArray()).any {
+            it.second == PackageManager.PERMISSION_DENIED &&
+                    !ActivityCompat.shouldShowRequestPermissionRationale(this, it.first)
+        }
+        val containsDenial = grantResults.any { it == PackageManager.PERMISSION_DENIED }
+        val allGranted = grantResults.all { it == PackageManager.PERMISSION_GRANTED }
+        when {
+            containsPermanentDenial -> {
+                // TODO: Handle permanent denial (e.g., show AlertDialog with justification)
+                // Note: The user will need to navigate to App Settings and manually grant
+                // permissions that were permanently denied
+            }
+            containsDenial -> {
+                requestRelevantRuntimePermissions()
+            }
+            allGranted && hasRequiredBluetoothPermissions() -> {
+                startBleScan()
+            }
+            else -> {
+                // Unexpected scenario encountered when handling permissions
+                recreate()
+            }
+        }
+    }
+
+    private fun startBleScan() {
+        if (!hasRequiredBluetoothPermissions()) {
+            requestRelevantRuntimePermissions()
+        } else { /* TODO: Actually perform scan */ }
+    }
+    private fun Activity.requestRelevantRuntimePermissions() {
+        if (hasRequiredBluetoothPermissions()) { return }
+        when {
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.S -> {
+                requestLocationPermission()
+            }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+                requestBluetoothPermissions()
+            }
+        }
+    }
+    private fun requestLocationPermission() = runOnUiThread {
+        AlertDialog.Builder(this)
+            .setTitle("Location permission required")
+            .setMessage(
+                "Starting from Android M (6.0), the system requires apps to be granted " +
+                        "location access in order to scan for BLE devices."
+            )
+            .setCancelable(false)
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSION_REQUEST_CODE
+                )
+            }
+            .show()
+    }
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun requestBluetoothPermissions() = runOnUiThread {
+        AlertDialog.Builder(this)
+            .setTitle("Bluetooth permission required")
+            .setMessage(
+                "Starting from Android 12, the system requires apps to be granted " +
+                        "Bluetooth access in order to scan for and connect to BLE devices."
+            )
+            .setCancelable(false)
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.BLUETOOTH_SCAN,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ),
+                    PERMISSION_REQUEST_CODE
+                )
+            }
+            .show()
     }
 }
